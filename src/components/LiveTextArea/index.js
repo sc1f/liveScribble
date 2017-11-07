4c108971eaff8eb95f12bf52f33ac79c81f4da3e
Jun Tan <tanjunyuan@hotmail.co.uk>
2017-11-07 13:19:07 -0600

    added some basic socket actions

--- src/components/LiveTextArea/index.js
+++ src/components/LiveTextArea/index.js
@@ -1,25 +1,31 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux'
 
-export default class extends Component {
+export default class LiveTextArea extends Component {
     constructor(props) {
         // always use constructor to create state for instance
         super(props);
         this.state = {
-            value: "",
-            last_edit_time: null,
-            open_time: null
+            content: 'write something/anything/whatever'
+            //last_edit_time: null,
+            //open_time: null
         };
+        //this.handleOnChange = this.handleOnChange.bind(this);
         this.handleInput = this.handleInput.bind(this);
         this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
     }
 
     componentDidMount() {
+        /*
         this.setState({
             last_save_time: null,
             open_time: Date.now(),
             show_save: false
-        });
+        }); */
         this.focusTextArea();
+        this._handleEditEvent();
     }
 
     handleKeyDown(event) {
@@ -27,47 +33,84 @@ export default class extends Component {
 
         let charCode = String.fromCharCode(event.which).toLowerCase();
 
-        if((event.ctrlKey && charCode === 's') || (event.metaKey && charCode === 's')) {
+        if ((event.ctrlKey && charCode === 's') || (event.metaKey && charCode === 's')) {
             event.preventDefault();
             this.setState({
                 last_save_time: Date.now(),
                 show_save: true
             });
 
-            setTimeout(function(self) {
+            setTimeout(function (self) {
                 self.state.show_save = false;
             }, 3000, this);
         }
     }
 
-    hideSave() {
-        this.setState({
-            show_save: false
-        });
+    handleSelect(event) {
+        let textarea = this.textarea_main;
+        let start = textarea.selectionStart;
+        let end = textarea.selectionEnd;
+        if (start === end) {
+            socket.emit('cursor moved', {
+                position: start,
+                edit_time: Date.now(),
+                user: 'jt123'
+            });
+        } else {
+            socket.emit('select', {
+                start: start,
+                end: end,
+                edit_time: Date.now(),
+                user: 'jt123'
+            });
+        }
+
     }
 
     focusTextArea() {
         this.textarea_main.focus();
     }
 
-    handleInput() {
-        this.setState({
-
+    handleInput(event) {
+        socket.emit('edit', {
+            content: event.target.value,
+            edit_time: Date.now(),
+            user: 'jt123'
         });
     }
 
+    _handleEditEvent() {
+        socket.on('edit', (content) => {
+            this.props.newEdit({
+                user: content.user,
+                edit_time: content.edit_time,
+
+            });
+        });
+    }
+
+    /*
+    handleOnChange() {
+
+    }*/
+
     render() {
-        return(
+        return (
             <div>
                 <textarea name="textarea-main"
                           id="textarea-main"
                           placeholder="write something/anything/whatever"
                           autoFocus
-                          onChange={ this.handleInput }
-                          //onKeyDown={ this.handleKeyDown }
-                          ref={(textarea_main) => { this.textarea_main = textarea_main; }}>
+                          onChange={this.handleInput}
+                          content={this.content}
+                          value={this.state.value}
+                          onMouseUp={this.handleSelect}
+                    //onKeyDown={ this.handleKeyDown }
+                          ref={(textarea_main) => {
+                              this.textarea_main = textarea_main;
+                          }}>
                 </textarea>
-                { this.state.show_save ? <p>last saved at { this.state.last_save_time } </p> : "" }
+                {this.state.show_save ? <p>last saved at {this.state.last_save_time} </p> : ""}
             </div>
         )
     }
--- tools/server.js
+++ tools/server.js
@@ -1,10 +1,10 @@
-import express from 'express';  
-import webpack from 'webpack';  
-import path from 'path';  
-import config from '../webpack.config.dev';  
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import config from '../webpack.config.dev';
 import open from 'open';
 import socket from 'socket.io';
-import { Server } from 'http';
+import {Server} from 'http';
 //import favicon from 'serve-favicon';
 
 /* eslint-disable no-console */
@@ -16,26 +16,41 @@ const compiler = webpack(config);
 const io = socket(server);
 
 
-app.use(require('webpack-dev-middleware')(compiler, {  
-  noInfo: true,
-  publicPath: config.output.publicPath
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler));  
+app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
-  console.log('get route');
-  res.sendFile(path.join( __dirname, '../src/index.html'));
+app.get('*', function (req, res) {
+    console.log('get route');
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-io.on('connection', function(socket) {
-    console.log('a user connected!');
+io.on('connection', function (socket) {
+    let new_user = {
+        username: '',
+        join_time: Date.now()
+    };
+
+    socket.on('edit', function(message) {
+       console.log('receiving edit: ' + message.content);
+    });
+
+    socket.on('select', function(message) {
+        console.log('selected: ' + message.start + ", " + message.end);
+    });
+
+    socket.on('cursor moved', function(data) {
+        console.log('new cursor position: ' + data.position);
+    });
 });
 
-server.listen(port, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    open(`http://localhost:${port}`);
-  }
+server.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open(`http://localhost:${port}`);
+    }
 });
